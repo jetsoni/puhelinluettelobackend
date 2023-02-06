b3cbafd8d115cc9668343a30987ea4adc553971d
Samuli Laaksonen <samulilaaksonen1@gmail.com>
2023-02-06 14:46:36 +0200

    build added

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 app.get('/', (req, res) => {
     res.send('<h1>hello world</h1>')
